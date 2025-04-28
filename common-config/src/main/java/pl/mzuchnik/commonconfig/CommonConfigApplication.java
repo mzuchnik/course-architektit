e11179fc92bc72287f0fee83c50a260b2bdbf948
Mateusz Żuchnik <mateusz.zuchnik97@gmail.com>
2025-04-28 16:04:49 +0200

    Added new config

---
+++ common-config/src/main/java/pl/mzuchnik/commonconfig/CommonConfigApplication.java
@@ -0,0 +1,12 @@
+package pl.mzuchnik.commonconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonConfigApplication.class, args);
+    }
+}
