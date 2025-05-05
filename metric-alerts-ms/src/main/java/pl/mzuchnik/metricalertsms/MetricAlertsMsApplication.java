0ef3f1746bee47fc41ef56fded8247392d9411f2
Mateusz Żuchnik <mateusz.zuchnik97@gmail.com>
2025-05-05 20:42:10 +0200

    Added Metric Alert MS

---
+++ metric-alerts-ms/src/main/java/pl/mzuchnik/metricalertsms/MetricAlertsMsApplication.java
@@ -0,0 +1,13 @@
+package pl.mzuchnik.metricalertsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricAlertsMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricAlertsMsApplication.class, args);
+    }
+
+}
---
+++ system-metrics-analyzer-ms/src/main/java/pl/mzuchnik/systemmetricsanalyzerms/SystemMetricsAnalyzerMsApplication.java
@@ -0,0 +1,13 @@
+package pl.mzuchnik.systemmetricsanalyzerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemMetricsAnalyzerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemMetricsAnalyzerMsApplication.class, args);
+	}
+
+}
